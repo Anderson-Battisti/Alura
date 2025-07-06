7af2b8937924090be6acd119dee936251f3a1d12
Anderson Battisti <anderson.battisti@universo.univates.br>
2025-07-05 22:49:58 -0300

    Audios added to all keys

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,6 +1,62 @@
-let button = document.querySelector( 'button' );
-
-button.addEventListener( 'click', function()
+function playPomAudio()
 {
-    alert( 'Pom clicked!' );
-} );
+    document.querySelector( '#som_tecla_pom' ).play();
+}
+
+document.querySelector( '.tecla_pom' ).onclick = playPomAudio;
+
+function playClapAudio()
+{
+    document.querySelector( '#som_tecla_clap' ).play();
+}
+
+document.querySelector( '.tecla_clap' ).onclick = playClapAudio;
+
+function playTimAudio()
+{
+    document.querySelector( '#som_tecla_tim' ).play();
+}
+
+document.querySelector( '.tecla_tim' ).onclick = playTimAudio;
+
+function playPuffAudio()
+{
+    document.querySelector( '#som_tecla_puff' ).play();
+}
+
+document.querySelector( '.tecla_puff' ).onclick = playPuffAudio;
+
+function playSplashAudio()
+{
+    document.querySelector( '#som_tecla_splash' ).play();
+}
+
+document.querySelector( '.tecla_splash' ).onclick = playSplashAudio;
+
+function playToimAudio()
+{
+    document.querySelector( '#som_tecla_toim' ).play();
+}
+
+document.querySelector( '.tecla_toim' ).onclick = playToimAudio;
+
+function playPshAudio()
+{
+    document.querySelector( '#som_tecla_psh' ).play();
+}
+
+document.querySelector( '.tecla_psh' ).onclick = playPshAudio;
+
+function playTicAudio()
+{
+    document.querySelector( '#som_tecla_tic' ).play();
+}
+
+document.querySelector( '.tecla_tic' ).onclick = playTicAudio;
+
+function playTomAudio()
+{
+    document.querySelector( '#som_tecla_tom' ).play();
+}
+
+document.querySelector( '.tecla_tom' ).onclick = playTomAudio;
